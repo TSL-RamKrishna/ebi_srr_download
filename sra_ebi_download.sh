1a4fb11c27195634409a2a0eb86b5f0506b723d8
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2021-01-22 13:34:44 +0000

    Update sra_ebi_download.sh

--- sra_ebi_download.sh
+++ sra_ebi_download.sh
@@ -2,7 +2,7 @@
 
 tsvfile=$1
 while read line; do
-  ftplink=$(echo $line | awk '{print $NF}');
-  wget $ftplink;  
+  ftplink=$(echo $line | awk '{print $(NF-1)}');
+  wget -c $ftplink;  
   
 done < <(tail -n+2 $tsvfile )
